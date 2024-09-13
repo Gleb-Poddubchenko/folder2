7b582dbc69362f5d5fab56473c71bc08d0617bd4
Glebasta <hlibpoddubchenko@gmail.com>
2024-09-13 19:58:52 +0200

    inner

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-console.dir(window.document.body.children)
+"use strict";
+
+const wrapperBtn = document.getElementById('wrapper-btn');
+
+const btn = document.querySelector('#wrapper-btn button');
+
+btn.addEventListener('click',({target})=>{ target.innerText = target.innerText === 'on'? 'off': 'on';
+});
 
 
-const [h1,p] = window.document.body.children;
-// console.log(h1,p);
+const closeBtn = document.getElementById('closeBtn');
 
-function hundlerClick(){
-    alert('h1 on klick')
-}
+closeBtn.addEventListener('click',(event)=>{
+    event.preventDefault();
+    const {target} = event;
+    const isClose = target.innerText ==='close';
+    target.innerText = isClose ? 'open' : 'close';
+    const display = isClose ?  'none':'block';
+    // nextElementSibling.remove();
+    target.nextElementSibling.style.setProperty('display',display);
+})
 
-h1.addEventListener('click',hundlerClick);
-// h1.removeEventListener('click',hundlerClick)
 
 
-h1.onclick = function(){
-    console.log('onklick 1');
-}
-h1.onclick = function(){
-    console.log('onklick 2');
-}
-h1.onclick = function(){
-    console.log('onklick 3');
-}
+
+
+
+
+
